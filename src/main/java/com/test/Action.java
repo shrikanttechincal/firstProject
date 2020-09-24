7eb684ca925d586bbe85e0a9bf40e34f12232ea3
shrikanttechincal <shrikantshetty5@gmail.com>
2020-09-24 16:17:41 +0530

    first commit

--- src/main/java/com/test/Action.java
+++ src/main/java/com/test/Action.java
@@ -7,26 +7,112 @@ import org.openqa.selenium.WebElement;
 
 public class Action {
 
-    public  WebDriver driver;
+    public WebDriver driver;
+
     //Constructor
-    public Action(WebDriver driver){
-        this.driver=driver;
+    public Action(WebDriver driver) {
+        this.driver = driver;
     }
 
     By popUpCross = By.xpath("//div[@class='mCRfo9']/div/div/button");
-    By insertField=By.xpath("//input[@name='q']");
-    By hitButton=By.xpath("//button[@class='vh79eN']");
+    By insertField = By.xpath("//input[@name='q']");
+    By hitButton = By.xpath("//button[@class='vh79eN']");
+    By secondpositionShoe = By.xpath("(//div[@class='_1vC4OE'])[2]");
+    By shoeSize1 = By.xpath("//a[contains(text(),'6')]");
+    By addCart1 = By.xpath("//button[@class='_2AkmmA _2Npkh4 _2MWPVK']");
+
+    By shoe2 = By.xpath("(//div[@class='_1vC4OE'])[3]");
+    By size2 = By.xpath("//a[contains(text(),'6')]");
+    By addCart2 = By.xpath("//button[@class='_2AkmmA _2Npkh4 _2MWPVK']");
+    By name1= By.xpath("//span[@class='_35KyD6']");
+    By price1=By.xpath("//div[@class='_1vC4OE _3qQ9m1']");
+    By name2= By.xpath("//span[@class='_35KyD6']");
+    By price2=By.xpath("//div[@class='_1vC4OE _3qQ9m1']");
+    By name3=By.xpath("//a[@class='_325-ji _3ROAwx']");
+    By price3=By.xpath("//span[@class='pMSy0p XU9vZa']");
+    By name4=By.xpath("//a[contains(text(),'ATHLETIC WORKS by Walmart Walking Shoes For Men')]");
+    By price4=By.xpath("//span[text()='239']");
+ By basketTotal=By.xpath("//span[contains(text(),'558')]");
+
+    public static void delay(){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public String basketTotal1() {
+        return driver.findElement(basketTotal).getText();
+    }
+
+
+public String pName1() {
+    return driver.findElement(name1).getText();
+}
+    public String pPrice1() {
+        return driver.findElement(price1).getText();
+    }
+    public String pName2() {
+        return driver.findElement(name2).getText();
+    }
+    public String pPrice2() {
+        return driver.findElement(price2).getText();
+    }
+
+
+    public String pName3() {
+        return driver.findElement(name3).getText();
+    }
+    public String pPrice3() {
+        return driver.findElement(price3).getText();
+    }
+    public String pName4() {
+        return driver.findElement(name4).getText();
+    }
+    public String pPrice4() {
+        return driver.findElement(price4).getText();
+    }
+
     //to close popup
-    public WebElement popUp(){
+    public WebElement popUp() {
         return driver.findElement(popUpCross);
     }
+
     //to give input field shoes
     public WebElement insertShoes() {
         return driver.findElement(insertField);
     }
+
     //to hit enter
     public WebElement hitSearch() {
         return driver.findElement(hitButton);
+    }
+
+    public WebElement secondPositionshoe() {
+
+        return driver.findElement(secondpositionShoe);
+    }
+
+    public WebElement shoeSize() {
+
+        return driver.findElement(shoeSize1);
+    }
+
+    public WebElement addTocart() {
+
+        return driver.findElement(addCart1);
+    }
+
+    public WebElement selectPosition3() {
+        return driver.findElement(shoe2);
+    }
+
+    public WebElement secondShoesize() {
+        return driver.findElement(size2);
+    }
+
+    public WebElement addTocart2() {
+        return driver.findElement(addCart2);
     }
 
 }
--- src/test/java/com/test/Flipkart1.java
+++ src/test/java/com/test/Flipkart1.java
@@ -12,10 +12,7 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Flipkart1 {
     WebDriver driver;
@@ -29,6 +26,12 @@ public class Flipkart1 {
     @Test
     public void test2() {
         Boolean b = getDetails();
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void test3() throws InterruptedException {
+        Boolean b = getDetails2();
         Assert.assertTrue(b);
     }
 
@@ -49,11 +52,9 @@ public class Flipkart1 {
         a.popUp().click();
         a.insertShoes().sendKeys(product);
         a.hitSearch().click();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        a.delay();///thread.sleep
+
       //Sorting of filter from excel
         List<WebElement> total = driver.findElements(By.xpath("//div[@class='_3ywJNQ']/div"));
         Iterator<WebElement> itTotal = total.iterator();
@@ -101,6 +102,237 @@ public class Flipkart1 {
 
     return a1.equals(a2);
     }
+    public Boolean data2(String product3, String filter3) throws InterruptedException {
+        product3 = product3.trim();
+
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\resource\\chromedriver.exe");
+
+        //lauching chrome in incognito mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        driver = new ChromeDriver(options);
+
+        //to make full screen
+        driver.manage().window().maximize();
+        driver.get("https://www.flipkart.com/");
+        Action a = new Action(driver);
+        a.popUp().click();
+        a.insertShoes().sendKeys(product3);
+        a.hitSearch().click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //Sorting of filter from excel
+        List<WebElement> total = driver.findElements(By.xpath("//div[@class='_3ywJNQ']/div"));
+        Iterator<WebElement> itTotal = total.iterator();
+        ArrayList a1 = new ArrayList();
+        ArrayList a2 = new ArrayList();
+        int count = 0;
+        int position = 0;
+        while (itTotal.hasNext()) {
+            count++;
+            String temp = itTotal.next().getText();
+            temp = temp.trim();
+            if (filter3.equalsIgnoreCase(temp)) {
+                position = count;
+                driver.findElement(By.xpath("//div[@class='_3ywJNQ']/div[" + position + "]")).click();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            System.out.println("temp = " + temp);
+        }
+        System.out.println("product = " + product3);
+        System.out.println("position = " + position);
+        System.out.println(filter3);
+
+
+        //sorting prices
+        List<WebElement> list1 = driver.findElements(By.xpath("//div[@class='_1vC4OE']"));
+        Iterator<WebElement> itr8=list1.iterator();
+        while(itr8.hasNext()){
+            WebElement temp1=itr8.next();
+            String temp2=temp1.getText();
+            //System.out.println("temp2"+  temp2);
+            String price_without_dollar=temp2.split("\u20B9")[1];
+            a1.add(price_without_dollar);
+        }
+
+        a2 = (ArrayList) a1.clone();
+        Collections.sort(a2);
+
+        System.out.println("a1 = "+a1);
+        System.out.println("a2 = "+a2);
+//        Assert.assertTrue(a1.equals(a2));
+
+        //selscting 2nd position shoes
+         a.secondPositionshoe().click();
+        String parenttitle= driver.getTitle();
+        System.out.println("parent"  +parenttitle);
+         Assert.assertEquals("Shoes - Buy Products Online at Best Price in India - All Categories | Flipkart.com",parenttitle);
+        Set<String> handle= driver.getWindowHandles();
+        int countt=handle.size();
+        System.out.println("count of no of tabs         "            +countt);
+        Iterator<String> it= handle.iterator();
+        String parentwindow1=it.next();
+        String childwindow1=it.next();
+
+        driver.switchTo().window(childwindow1);
+        String childtitle= driver.getTitle();
+        System.out.println("chiltitle"    +childtitle);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        a.shoeSize().click();
+        //assertion1----------------------------------------
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String assertname1=a.pName1();
+
+        String assert1=a.pPrice1();
+        String Pprice1=assert1.split("\u20B9")[1];
+
+
+        System.out.println("assertname1  :  "   +assertname1 );
+
+        System.out.println("Pprice1 = "   +Pprice1 );
+        int pro1=Integer.parseInt(Pprice1);
+        System.out.println("After converssion first product  price to int = "   +pro1 );
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        a.addTocart().click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //again back to parent page
+       driver.switchTo().window(parentwindow1);
+        System.out.println("parentwindow1 id"   +parentwindow1);
+        System.out.println("select another shoes from parernt window");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        a.selectPosition3().click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        //switching to new child window
+
+        Set<String> handle2= driver.getWindowHandles();
+        Iterator<String> it3= handle2.iterator();
+
+            String p1= it3.next();
+            String c1=it3.next();
+            String c2=it3.next();
+            System.out.println("p1 id"   +p1);
+            System.out.println("c1 id"   +c1);
+           System.out.println("c2 id"   +c2);
+        driver.switchTo().window(c2);
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String assertname2=a.pName2();
+        String assert2=a.pPrice2();
+        String Pprice2=assert2.split("\u20B9")[1];
+
+
+        System.out.println("assertname2  :  "   +assertname2 );
+
+        System.out.println("Pprice2 = "   +Pprice2 );
+
+        int pro2=Integer.parseInt(Pprice2);
+        System.out.println("After converssion first product  price to int = "   +pro2 );
+    a.secondShoesize().click();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        a.addTocart2().click();
+        System.out.println("second shoes selected");
+
+
+//Basket assertion
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String assertname3=a.pName3();
+        String assert3=a.pPrice3();
+        String Pprice3=assert3.split("\u20B9")[1];
+        System.out.println("assertname3  :  "   +assertname3 );
+
+        System.out.println("Pprice3 = "   +Pprice3 );
+        int pro3=Integer.parseInt(Pprice3);
+        System.out.println("After converssion first product  price to int = "   +pro3 );
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String assertname4=a.pName4();
+        String assert4=a.pPrice4();
+        String Pprice4=assert4.split("\u20B9")[1];
+        System.out.println("assertname4  :  "   +assertname4 );
+
+        System.out.println("Pprice4 = "   +Pprice4);
+        int pro4=Integer.parseInt(Pprice1);
+        System.out.println("After converssion first product  price to int = "   +pro4 );
+        int add1=pro1+pro2;
+        System.out.println("Addition of selection= "   +add1 );
+
+
+  Assert.assertEquals(Pprice1,Pprice3);
+  System.out.println("1st product price tested propely");
+        Assert.assertEquals(Pprice2,Pprice4);
+        System.out.println("2nd product price tested propely");
+        ///basket total
+     String bt1=   a.basketTotal1();
+
+        String bt2=bt1.split("\u20B9")[1];
+
+        int basketpricetotal=Integer.parseInt(bt2);
+        System.out.println("basketpricetotal  :  "   +basketpricetotal);
+        System.out.println("comparison");
+        Assert.assertEquals(add1,basketpricetotal);
+
+
+
+
+        return a1.equals(a2);
+    }
+
+
+
 
 //data fetching from excel
     public Boolean getDetails() {
@@ -113,4 +345,18 @@ public class Flipkart1 {
 
         return data(product, filter2);
     }
+
+    public Boolean getDetails2() throws InterruptedException {
+
+        ExcelFile configuration = new ExcelFile(System.getProperty("user.dir") + "\\src\\test\\resource\\Flipkart.xlsx");
+        //  int rows = configuration.getRowCount(0);
+        Object[][] signin_credentials = new Object[1][2];
+        String product3 = configuration.getData(0, 1, 0);
+        String filter3 = configuration.getData(0, 1, 1);
+
+
+        return data2(product3,filter3);
+    }
+
+
 }
